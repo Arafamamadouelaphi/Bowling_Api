ab64c11ea56815940a1e869ecb919b7b2994076d
Victor Pérez Ngounou <victor_perez.ngounou@etu.uca.fr>
2023-02-01 11:26:50 +0100

    commit

--- Sources/DTOs/JoueurDTO.cs
+++ Sources/DTOs/JoueurDTO.cs
@@ -9,8 +9,6 @@ namespace DTOs
     public class JoueurDTO
     {
         #region Properties
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public string Pseudo { get; set; }
         public ICollection<PartieDTO> PartieDTO { get; set; } = new List<PartieDTO>();
