9d31e48f96321253d10c60d553a37d62378221c4
etudiant <mamadouarafa35@gmail.com>
2023-02-12 08:35:25 +0100

    cmtstub

--- Sources/BowlingStub/StubJoueur.cs
+++ Sources/BowlingStub/StubJoueur.cs
@@ -7,9 +7,14 @@ namespace BowlingStub
     public class StubJoueur : IDataManager<Joueur>
 
     {
+        // Liste pour stocker les joueurs
         private List<Joueur> listJoueurs = new List<Joueur>();
 
-
+        /// <summary>
+        /// Ajouter un joueur à la liste
+        /// </summary>
+        /// <param name="data">Joueur à ajouter</param>
+        /// <returns>Retourne `true` si le joueur a été ajouté avec succès, sinon `false`</returns>
         public async Task<bool> Add(Joueur data)
         {
             if (data != null)
@@ -19,7 +24,11 @@ namespace BowlingStub
             }
             return false;
         }
-
+        /// <summary>
+        /// Supprimer un joueur de la liste
+        /// </summary>
+        /// <param name="data">Joueur à supprimer</param>
+        /// <returns>Retourne `true` si le joueur a été supprimé avec succès, sinon `false`</returns>
         public async Task<bool> Delete(Joueur data)
         {
             if (data != null)
@@ -29,7 +38,11 @@ namespace BowlingStub
             }
             return false;
         }
-
+        /// <summary>
+        /// Créer une liste de joueurs
+        /// </summary>
+        /// <param name="n">Nombre de joueurs à créer</param>
+        /// <returns>Liste de joueurs</returns>
         public async Task<IEnumerable<Joueur>> GetAll()
         {
             return listJoueurs;
@@ -43,12 +56,20 @@ namespace BowlingStub
             }
             return listJoueurs;
         }
-        ///ged 
+        // <summary>
+        /// Obtenir un joueur en fonction de son identifiant
+        /// </summary>
+        /// <param name="id">Identifiant du joueur</param>
+        /// <returns>Joueur correspondant à l'identifiant</returns>
         public async Task<Joueur >GetDataWithId (int id)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Obtenir un joueur en fonction de son nom
+        /// </summary>
+        /// <param name="name">Nom du joueur</param>
+        /// <returns>Joueur correspondant au nom</returns>
         public async Task<Joueur> GetDataWithName(string name)
         {
             throw new NotImplementedException();
